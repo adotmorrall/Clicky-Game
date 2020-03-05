1bf8e589cd1220d605db95b9a30477daa4e1581f
Andre <andre.morrall@icloud.com>
2020-03-04 23:19:26 -0600

    Updates

---
+++ precache-manifest.1d79b744d8989f973589c676b0d04d2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e1f8b30b4002ad71d5ba0b815b5599",
+    "url": "/index.html"
+  },
+  {
+    "revision": "318da5d3c3dc64cee848",
+    "url": "/static/css/main.e3800bca.chunk.css"
+  },
+  {
+    "revision": "f956f48d66b32262784a",
+    "url": "/static/js/2.67350af0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.67350af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318da5d3c3dc64cee848",
+    "url": "/static/js/main.580170dc.chunk.js"
+  },
+  {
+    "revision": "b3327f83cfce66dff033",
+    "url": "/static/js/runtime-main.468e39a4.js"
+  }
+]);
