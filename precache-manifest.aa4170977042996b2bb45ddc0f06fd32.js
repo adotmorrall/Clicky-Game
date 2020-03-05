8f24db0d2291f8216f3293d1dae67330935a650f
Andre <andre.morrall@icloud.com>
2020-03-05 00:02:57 -0600

    Updates

---
+++ precache-manifest.aa4170977042996b2bb45ddc0f06fd32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "594e4ad855ca8b08408bfc8a78c57ac3",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b1a9aa42d0b6b5c67263",
+    "url": "/clicky-game/static/css/main.6267affb.chunk.css"
+  },
+  {
+    "revision": "f956f48d66b32262784a",
+    "url": "/clicky-game/static/js/2.67350af0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.67350af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1a9aa42d0b6b5c67263",
+    "url": "/clicky-game/static/js/main.157cec0a.chunk.js"
+  },
+  {
+    "revision": "c009a0a1e3686787466f",
+    "url": "/clicky-game/static/js/runtime-main.4231084e.js"
+  }
+]);
